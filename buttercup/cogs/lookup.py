812e56b230ffbb6af0df07e522ef07454d029a58
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-06-13 21:50:11 +0200

    Apply basic normalization to lookup URLs

--- buttercup/cogs/lookup.py
+++ buttercup/cogs/lookup.py
@@ -1,6 +1,9 @@
 from discord.ext.commands import Cog
 from discord_slash import SlashContext, cog_ext
 from discord_slash.utils.manage_commands import create_option
+from urllib.parse import urlparse
+
+from typing import Optional
 
 from buttercup.bot import ButtercupBot
 
@@ -9,6 +12,27 @@ class Lookup(Cog):
     def __init__(self, bot: ButtercupBot) -> None:
         """Initialize the Lookup cog."""
         self.bot = bot
+
+    @staticmethod
+    def _parse_reddit_url(reddit_url_str: str) -> Optional[str]:
+        """
+        Tries to parse and normalize the given Reddit URL.
+
+        :returns: The normalized Reddit URL or None if the parsing failed.
+        """
+        parse_result = urlparse(reddit_url_str)
+
+        if "reddit" not in parse_result.netloc:
+            return None
+
+        # On Blossom, all URLs end with a slash
+        path = parse_result.path
+        if not path.endswith("/"):
+            path += "/"
+
+        # Reformat URL in the format that Blossom uses
+        return f"https://reddit.com{path}"
+
 
     @cog_ext.cog_slash(
         name="lookup",
@@ -25,7 +49,14 @@ class Lookup(Cog):
     async def _lookup(self, ctx: SlashContext, reddit_url: str) -> None:
         """Look up the post with the given URL."""
 
-        await ctx.send(f'Looking up post with URL "{reddit_url}"')
+        normalized_url = Lookup._parse_reddit_url(reddit_url)
+
+        if normalized_url is None:
+            await ctx.send(f"I don't recognize '{reddit_url}' as valid Reddit URL. Please provide a link to "
+                           "either a post on a r/TranscribersOfReddit, on a partner sub or a transcription.")
+            return
+
+        await ctx.send(f'Looking up post with URL "{normalized_url}"')
 
 
 def setup(bot: ButtercupBot) -> None:
