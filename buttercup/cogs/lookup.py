10cc1954f17cd2c4d8ae768a40beb5bdb40ae427
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-06-13 22:33:44 +0200

    Add basic submission lookup functionality

--- buttercup/cogs/lookup.py
+++ buttercup/cogs/lookup.py
@@ -6,12 +6,14 @@ from urllib.parse import urlparse
 from typing import Optional
 
 from buttercup.bot import ButtercupBot
+from blossom_wrapper import BlossomAPI, BlossomStatus
 
 
 class Lookup(Cog):
-    def __init__(self, bot: ButtercupBot) -> None:
+    def __init__(self, bot: ButtercupBot, blossom: BlossomAPI) -> None:
         """Initialize the Lookup cog."""
         self.bot = bot
+        self.blossom = blossom
 
     @staticmethod
     def _parse_reddit_url(reddit_url_str: str) -> Optional[str]:
@@ -32,7 +34,6 @@ class Lookup(Cog):
 
         # Reformat URL in the format that Blossom uses
         return f"https://reddit.com{path}"
-
 
     @cog_ext.cog_slash(
         name="lookup",
@@ -56,12 +57,26 @@ class Lookup(Cog):
                            "either a post on a r/TranscribersOfReddit, on a partner sub or a transcription.")
             return
 
-        await ctx.send(f'Looking up post with URL "{normalized_url}"')
+        response = self.blossom.get_submission(url=normalized_url)
+        data = response.data
+
+        if response.status != BlossomStatus.ok or data is None or len(data) == 0:
+            await ctx.send(f"""I couldn't find a post with URL "{normalized_url}", sorry.""")
+            return
+
+        submission = data[0]
+
+        await ctx.send(f'I found your post!\n{submission["url"]}\n{submission["tor_url"]}')
 
 
 def setup(bot: ButtercupBot) -> None:
     """Set up the Lookup cog."""
-    bot.add_cog(Lookup(bot))
+    cog_config = bot.config["Blossom"]
+    email = cog_config.get("email")
+    password = cog_config.get("password")
+    api_key = cog_config.get("api_key")
+    blossom = BlossomAPI(email=email, password=password, api_key=api_key)
+    bot.add_cog(Lookup(bot, blossom))
 
 
 def teardown(bot: ButtercupBot) -> None:
