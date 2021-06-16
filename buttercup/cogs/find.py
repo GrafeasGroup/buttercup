3bf3fd89da6c4a2d99294acc884ca090a955ed78
Joe Kaufeld <joe@kenzie.academy>
2021-06-15 22:16:08 -0400

    fix scope issues and add handling for transcribot

--- buttercup/cogs/find.py
+++ buttercup/cogs/find.py
@@ -1,4 +1,3 @@
-import logging
 from typing import Dict, Optional, Union
 from urllib.parse import urlparse
 
@@ -50,34 +49,60 @@ def limit_str(text: str, limit: Optional[int] = None) -> str:
     return f"{text[:(limit - 3)]}..."
 
 
+def get_clean_transcription(data: Dict) -> str:
+    """
+    Get the content of the transcription, without header and footer.
+
+    Because there can be an OCR transcription as well, then we'll
+    try and return the human one but will fall through to the OCR
+    one if we can't find any.
+    """
+    key = "transcription" if "transcription" in data else "ocr"
+    transcription_text = data[key]["text"]
+
+    if get_id_from_url(data[key]["author"]) == 3:
+        # The author is transcribot and doesn't use our format
+        return transcription_text
+
+    parts = transcription_text.split("---")
+    if len(parts) < 3:
+        return transcription_text
+
+    # Discard header and footer
+    return "---".join(parts[1:-1]).strip()
+
+
 class Find(Cog):
     def __init__(self, bot: ButtercupBot, blossom_api: BlossomAPI) -> None:
         """Initialize the Find cog."""
         self.bot = bot
         self.blossom_api = blossom_api
 
-    def to_embed(self) -> Embed:  # noqa: C901
+    def to_embed(self, data: Dict) -> Embed:  # noqa: C901
         """Convert the submission to a Discord embed."""
         color = Color.from_rgb(255, 176, 0)  # Orange
         status: str = "Unclaimed"
 
-        tr_link = f"[Link]({self.transcription['url']})" if self.transcription else None
+        if "transcription" in data:
+            tr_link = f"[Link]({data['transcription']['url']})"
+        else:
+            tr_link = None
 
-        if self.submission["completed_by"]:
+        if data["submission"].get("completed_by"):
             color = Color.from_rgb(148, 224, 68)  # Green
 
-            if self.author:
+            if "author" in data:
                 status = "Completed by {}".format(
-                    DISCORD_USERNAME_LINK.format(self.author["username"])
+                    DISCORD_USERNAME_LINK.format(data["author"]["username"])
                 )
             else:
                 status = "Completed"
-        elif self.submission["claimed_by"]:
+        elif data["submission"].get("claimed_by"):
             color = Color.from_rgb(13, 211, 187)  # Cyan
 
-            if self.author:
+            if "author" in data:
                 status = "Claimed by {}".format(
-                    DISCORD_USERNAME_LINK.format(self.author["username"])
+                    DISCORD_USERNAME_LINK.format(data["author"]["username"])
                 )
             else:
                 status = "Claimed"
@@ -87,63 +112,65 @@ class Find(Cog):
             .add_field(name="Status", value=status)
             .add_field(
                 name="OCR",
-                value="Yes" if self.submission["has_ocr_transcription"] else "No",
+                value="Yes"
+                if data["submission"].get("has_ocr_transcription")
+                else "No",
             )
             .add_field(
-                name="Archived", value="Yes" if self.submission["archived"] else "No"
+                name="Archived",
+                value="Yes" if data["submission"].get("archived") else "No",
             )
         )
 
-        if self.transcription:
-            embed.description = limit_str(self.transcription["text"], 200)
-        if self.submission.get("content_url"):
-            embed.set_image(url=self.submission["content_url"])
-        if self.submission.get("tor_url"):
+        if "transcription" in data or "ocr" in data:
+            embed.description = limit_str(get_clean_transcription(data), 200)
+        if data["submission"].get("content_url"):
+            embed.set_image(url=data["submission"]["content_url"])
+        if data["submission"].get("tor_url"):
             embed.add_field(
-                name="ToR Post", value=f"[Link]({self.submission['tor_url']})"
+                name="ToR Post", value=f"[Link]({data['submission']['tor_url']})"
             )
-        if self.submission.get("url"):
+        if data["submission"].get("url"):
             embed.add_field(
-                name="Partner Post", value=f"[Link]({self.submission.get('url')})"
+                name="Partner Post", value=f"[Link]({data['submission']['url']})"
             )
         if tr_link:
             embed.add_field(name="Transcription", value=tr_link)
-        subreddit = (
-            self.submission["url"].split("/")[4] if self.submission["url"] else None
-        )
-        if subreddit:
+        if subreddit := (
+            data["submission"]["url"].split("/")[4]
+            if "url" in data["submission"]
+            else None
+        ):
             embed.set_author(
                 name=f"r/{subreddit}", url=f"https://reddit.com/r/{subreddit}"
             )
 
         return embed
 
-    def _clear(self) -> None:
-        """Reset the local scope caches for a new search."""
-        self.transcription: Union[Dict, None] = None
-        self.submission: Union[Dict, None] = None
-        self.author: Union[Dict, None] = None
-
-    def get_transcription(self) -> None:
+    def get_transcription(self, data: Dict) -> Dict:
         """Get the target transcription from Blossom."""
-        for option in self.submission["transcription_set"]:
+        for option in data["submission"]["transcription_set"]:
             response = self.blossom_api.get_transcription(id=get_id_from_url(option))
-            logging.info(response.data[0])
-            if response.data[0]["author"] == self.submission["completed_by"]:
-                self.transcription = response.data[0]
-                return
+            if response.data[0]["author"] == data["submission"].get("completed_by"):
+                data["transcription"] = response.data[0]
+            elif get_id_from_url(response.data[0]["author"]) == 3:
+                # it's transcribot
+                data["ocr"] = response.data[0]
 
-    def get_author(self) -> None:
+        return data
+
+    def get_author(self, data: Dict) -> Dict:
         """Retrieve the author from the submission or the transcription."""
-        if self.transcription:
-            source_url = self.transcription["author"]
+        if data.get("transcription"):
+            source_url = data["transcription"]["author"]
         else:
-            source_url = self.submission["claimed_by"]
-        self.author = self.blossom_api.get(
+            source_url = data["submission"]["claimed_by"]
+        data["author"] = self.blossom_api.get(
             "volunteer/", params={"id": get_id_from_url(source_url)}
         ).json()["results"][0]
+        return data
 
-    def get_submission_from_url(self, reddit_url: str) -> Union[Dict, None]:
+    def get_submission_from_url(self, reddit_url: str, data: Dict) -> Dict:
         """
         Try to get the submission corresponding to the given Reddit URL.
 
@@ -151,7 +178,6 @@ class Find(Cog):
         on r/ToR, or it can be a transcription link.
         """
         # Determine what kind of link we're dealing with:
-        logging.critical(len(reddit_url.split("/")))
         if len(reddit_url.split("/")) <= 9:
             # It's a link to a submission, either on ToR or a partner
             urltype = "tor_url" if "TranscribersOfReddit" in reddit_url else "url"
@@ -161,16 +187,19 @@ class Find(Cog):
             # This means that the path is longer, because of the added comment ID
             tr_response = self.blossom_api.get_transcription(url=reddit_url)
             if tr_response.status == BlossomStatus.not_found:
-                return None
-            self.transcription = tr_response.data[0]
+                data["submission"] = None
+                return data
+            data["transcription"] = tr_response.data[0]
 
             # We don't have direct access to the submission ID, so we need to
             # extract it from the submission URL
-            submission_id = get_id_from_url(self.transcription["submission"])
+            submission_id = get_id_from_url(data["transcription"]["submission"])
             result = self.blossom_api.get_submission(id=submission_id)
 
-        logging.critical(result.data[0] if result.status == BlossomStatus.ok else None)
-        self.submission = result.data[0] if result.status == BlossomStatus.ok else None
+        data["submission"] = (
+            result.data[0] if result.status == BlossomStatus.ok else None
+        )
+        return data
 
     @cog_ext.cog_slash(
         name="find",
@@ -187,7 +216,6 @@ class Find(Cog):
     )
     async def _find(self, ctx: SlashContext, reddit_url: str) -> None:
         """Find the post with the given URL."""
-        self._clear()
         if (reddit_url := normalize_url(reddit_url)) is None:
             await ctx.send(
                 "Sorry, that doesn't look like a URL I can work with."
@@ -199,8 +227,10 @@ class Find(Cog):
         # We will edit this message later with the actual content.
         msg = await ctx.send(f"Looking for post <{reddit_url}>...")
 
-        self.get_submission_from_url(reddit_url)
-        if self.submission is None:
+        data: Dict = {}
+
+        data: Dict[str, Dict] = self.get_submission_from_url(reddit_url, data)
+        if data["submission"] is None:
             await msg.edit(
                 content=(
                     f"Sorry, I couldn't find a post with the URL <{reddit_url}>."
@@ -209,18 +239,18 @@ class Find(Cog):
             )
             return
 
-        await msg.edit(content="I found the post!", embed=self.to_embed())
+        await msg.edit(content="I found the post!", embed=self.to_embed(data))
 
         # If we started with a transcription link, this will be set already.
-        if not self.transcription:
-            self.get_transcription()
+        if "transcription" not in data:
+            data: Dict[str, Dict] = self.get_transcription(data)
 
-        await msg.edit(content="I found the post!", embed=self.to_embed())
+        await msg.edit(content="I found the post!", embed=self.to_embed(data))
 
-        if self.transcription or self.submission["claimed_by"]:
-            self.get_author()
+        if data.get("transcription") or data["submission"].get("claimed_by"):
+            data: Dict[str, Dict] = self.get_author(data)
 
-        await msg.edit(content="I found the post!", embed=self.to_embed())
+        await msg.edit(content="I found the post!", embed=self.to_embed(data))
 
 
 def setup(bot: ButtercupBot) -> None:
