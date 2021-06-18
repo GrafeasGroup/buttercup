698a77f1e902f83e2d9d932a0af275b44acc210d
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-06-18 23:29:20 +0200

    Switch to async praw, handle invalid subreddit names

--- buttercup/cogs/rules.py
+++ buttercup/cogs/rules.py
@@ -1,7 +1,6 @@
-from datetime import datetime
-
-import praw
-from discord import Color, Embed
+import asyncpraw
+from asyncprawcore import Redirect, NotFound
+from discord import Embed
 from discord.ext.commands import Cog
 from discord_slash import SlashContext, cog_ext
 from discord_slash.utils.manage_commands import create_option
@@ -10,7 +9,7 @@ from buttercup.bot import ButtercupBot
 
 
 class Rules(Cog):
-    def __init__(self, bot: ButtercupBot, reddit_api: praw.Reddit) -> None:
+    def __init__(self, bot: ButtercupBot, reddit_api: asyncpraw.Reddit) -> None:
         """Initialize the Rules cog."""
         self.bot = bot
         self.reddit_api = reddit_api
@@ -32,11 +31,31 @@ class Rules(Cog):
         # We will edit this later with the actual content
         msg = await ctx.send(f"Getting the rules for r/{subreddit}...")
 
+        sub = await self.reddit_api.subreddit(subreddit)
+
+        embed = Embed(title=f"Rules for r/{subreddit}")
+
+        try:
+            async for rule in sub.rules:
+                # The value field is not allowed to be a blank string
+                # So we just repeat the name of the rule if it is not provided
+                embed.add_field(name=rule.short_name, value=rule.description or rule.short_name, inline=False)
+        except Redirect:
+            # Sometimes Reddit redirects to the subreddit search
+            await msg.edit(content=f"I couldn't find a sub named r/{subreddit}. Please check the spelling.")
+            return
+        except NotFound:
+            # Sometimes it throws a not found exception, e.g. if a character isn't allowed
+            await msg.edit(content=f"I couldn't find a sub named r/{subreddit}. Please check the spelling.")
+            return
+
+        await msg.edit(content="Here are the rules!", embed=embed)
+
 
 def setup(bot: ButtercupBot) -> None:
     """Set up the Rules cog."""
     reddit_config = bot.config["Reddit"]
-    reddit_api = praw.Reddit(
+    reddit_api = asyncpraw.Reddit(
         client_id=reddit_config["client_id"],
         client_secret=reddit_config["client_secret"],
         user_agent=reddit_config["user_agent"],
