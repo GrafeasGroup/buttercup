09a939721ff78958138567e8180c280552c54898
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-06-24 15:02:43 +0200

    Color the heatmap table according to the intensity of the value

--- buttercup/cogs/heatmap.py
+++ buttercup/cogs/heatmap.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import io
 from typing import Optional, Dict, List
 
@@ -9,11 +8,33 @@ from discord import File
 from discord.ext.commands import Cog
 from discord_slash import SlashContext, cog_ext
 from discord_slash.utils.manage_commands import create_option
+from matplotlib.font_manager import FontProperties
 
 from buttercup.bot import ButtercupBot
 
 
-def init_heatmap_dict() -> Dict[int, Dict[int, int]]:
+# Colors to use in the plots
+background_color = "#36393f"  # Discord background color
+text_color = "white"
+line_color = "white"
+intensity_colors = [
+    "#07102c",
+    "#0a1842",
+    "#0e2058",
+    "#11286e",
+    "#153184",
+    "#18399a",
+    "#1c41b0",
+    "#1f49c7",
+    "#2251dd",
+    "#3862e0",
+]
+
+
+Heatmap = Dict[int, Dict[int, int]]
+
+
+def init_heatmap_dict() -> Heatmap:
     """Get the initial dictionary for the heatmap data.
 
     For every day (1 = Monday, 7 = Sunday), it contains a dict entry
@@ -32,7 +53,7 @@ def init_heatmap_dict() -> Dict[int, Dict[int, int]]:
     return heatmap_dict
 
 
-def get_heatmap_dict(data: List[Dict[str, int]]) -> Dict[int, Dict[int, int]]:
+def get_heatmap_dict(data: List[Dict[str, int]]) -> Heatmap:
     """Populate the heatmap dictionary with the data returned by Blossom."""
     heatmap_dict = init_heatmap_dict()
 
@@ -46,7 +67,7 @@ def get_heatmap_dict(data: List[Dict[str, int]]) -> Dict[int, Dict[int, int]]:
     return heatmap_dict
 
 
-def get_cell_text(heatmap_dict: Dict[int, Dict[int, int]]) -> List[List[str]]:
+def get_cell_texts(heatmap_dict: Heatmap) -> List[List[str]]:
     """Get the text for each cell of the table."""
     rows = []
 
@@ -64,12 +85,51 @@ def get_cell_text(heatmap_dict: Dict[int, Dict[int, int]]) -> List[List[str]]:
     return rows
 
 
+def get_max_value(heatmap_dict: Heatmap) -> int:
+    """Calculate the maximum value in the heatmap."""
+    max_value = 0
+
+    for day in heatmap_dict:
+        day_data = heatmap_dict[day]
+
+        for hour in day_data:
+            hour_data = day_data[hour]
+
+            max_value = max(max_value, hour_data)
+
+    return max_value
+
+
+def get_cell_colors(heatmap_dict: Heatmap) -> List[List[str]]:
+    """Get the color for each cell of the table."""
+    rows = []
+    max_value = get_max_value(heatmap_dict)
+
+    for day in heatmap_dict:
+        cols = []
+        day_data = heatmap_dict[day]
+
+        for hour in day_data:
+            value = day_data[hour]
+
+            if value == 0:
+                cols.append(background_color)
+                continue
+
+            percentage = value / max_value
+            intensity = round(percentage * (len(intensity_colors) - 1))
+            color = intensity_colors[intensity]
+            cols.append(color)
+
+        rows.append(cols)
+
+    return rows
+
+
 def create_file_from_heatmap(
     heatmap_dict: Dict[int, Dict[int, int]], username: str,
 ) -> File:
     """Create a Discord file containing the heatmap table."""
-    background_color = "#36393f"  # Discord background color
-
     days = [
         "Monday",
         "Tuesday",
@@ -80,10 +140,11 @@ def create_file_from_heatmap(
         "Sunday",
     ]
     hours = ["{:02d}".format(hour) for hour in range(0, 24)]
-    cells = get_cell_text(heatmap_dict)
+    cells = get_cell_texts(heatmap_dict)
 
     row_colors = [background_color for _ in range(0, 7)]
     col_colors = [background_color for _ in range(0, 24)]
+    cell_colors = get_cell_colors(heatmap_dict)
 
     fig, ax = plt.subplots()
 
@@ -91,17 +152,27 @@ def create_file_from_heatmap(
     ax.axis("off")
     ax.axis("tight")
 
-    ax.table(
+    table = ax.table(
         rowLoc="right",
         rowLabels=days,
         rowColours=row_colors,
         colLoc="center",
         colLabels=hours,
         colColours=col_colors,
+        cellLoc="center",
         cellText=cells,
+        cellColours=cell_colors,
         loc="center",
-        edges="",
     )
+
+    for (row, col), cell in table.get_celld().items():
+        # Make headers bold
+        if (row == 0) or (col == -1):
+            cell.set_text_props(fontproperties=FontProperties(weight='bold'))
+
+        # Remove cell edges
+        # Setting edges="open" removes the cell colors for some reason
+        cell.set_linewidth(0)
 
     heatmap_table = io.BytesIO()
     plt.savefig(heatmap_table, format="png")
@@ -160,12 +231,6 @@ def setup(bot: ButtercupBot) -> None:
     blossom_api = BlossomAPI(email=email, password=password, api_key=api_key)
 
     # Initialize PyPlot
-
-    # Colors to use in the plots
-    background_color = "#36393f"  # Discord background color
-    text_color = "white"
-    line_color = "white"
-
     # Global settings for the plots
     plt.rcParams["figure.facecolor"] = background_color
     plt.rcParams["axes.facecolor"] = background_color
