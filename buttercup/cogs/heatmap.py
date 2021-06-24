c2f8555eb5d040f4ffef95c089a1d7191276df4d
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-06-24 12:44:48 +0200

    Aggregate heatmap data in dictionary

--- buttercup/cogs/heatmap.py
+++ buttercup/cogs/heatmap.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Dict, List
 
 import matplotlib.pyplot as plt
 from blossom_wrapper import BlossomAPI
@@ -10,6 +10,39 @@ from discord_slash.utils.manage_commands import create_option
 from buttercup.bot import ButtercupBot
 
 
+def init_heatmap_dict() -> Dict[int, Dict[int, int]]:
+    """Get the initial dictionary for the heatmap data.
+
+    For every day (1 = Monday, 7 = Sunday), it contains a dict entry
+    with an entry 0 for every hour.
+    """
+    heatmap_dict = {}
+
+    for day in range(1, 8):
+        day_dict = {}
+
+        for hour in range(0, 24):
+            day_dict[hour] = 0
+
+        heatmap_dict[day] = day_dict
+
+    return heatmap_dict
+
+
+def get_heatmap_dict(data: List[Dict[str, int]]) -> Dict[int, Dict[int, int]]:
+    """Populate the heatmap dictionary with the data returned by Blossom."""
+    heatmap_dict = init_heatmap_dict()
+
+    for entry in data:
+        day = entry["day"]
+        hour = entry["hour"]
+        count = entry["count"]
+
+        heatmap_dict[day][hour] += count
+
+    return heatmap_dict
+
+
 class Heatmap(Cog):
     def __init__(self, bot: ButtercupBot, blossom_api: BlossomAPI) -> None:
         """Initialize the Heatmap cog."""
@@ -17,7 +50,8 @@ class Heatmap(Cog):
         self.blossom_api = blossom_api
 
     @cog_ext.cog_slash(
-        name="heatmap", description="Display the activity heatmap for the given user.",
+        name="heatmap",
+        description="Display the activity heatmap for the given user.",
         options=[
             create_option(
                 name="username",
@@ -31,14 +65,18 @@ class Heatmap(Cog):
         """Generate a heatmap for the given user."""
         msg = await ctx.send(f"Generating a heatmap for u/{username}...")
 
-        response = self.blossom_api.get("volunteer/heatmap/", params={"username": username})
+        response = self.blossom_api.get(
+            "volunteer/heatmap/", params={"username": username}
+        )
 
         if response.status_code != 200:
             await msg.edit(content=f"User u/{username} not found!")
             return
 
         data = response.json()
-        await msg.edit(content=f"Data: {data}")
+        heatmap_dict = get_heatmap_dict(data)
+
+        await msg.edit(content=f"Data: {heatmap_dict}")
 
 
 def setup(bot: ButtercupBot) -> None:
