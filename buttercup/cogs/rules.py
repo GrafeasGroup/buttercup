95c7b14e74fff47347884c70699a0a80a8fa56e6
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-07-01 13:23:25 +0200

    Extract common rule command functionality

--- buttercup/cogs/rules.py
+++ buttercup/cogs/rules.py
@@ -7,6 +7,7 @@ from asyncprawcore import Forbidden, NotFound, Redirect
 from discord import Embed
 from discord.ext.commands import Cog
 from discord_slash import SlashContext, cog_ext
+from discord_slash.model import SlashMessage
 from discord_slash.utils.manage_commands import create_option
 
 from buttercup.bot import ButtercupBot
@@ -52,11 +53,62 @@ def is_pi_rule(rule: Rule) -> bool:
     )
 
 
+async def send_rules_message(
+    msg: SlashMessage,
+    rules: List[Rule],
+    subreddit: str,
+    start_time: datetime,
+    localization_key: str,
+) -> None:
+    """Send an embed containing the rules to the user."""
+    embed = Embed(title=i18n[localization_key]["embed_title"].format(subreddit))
+
+    if len(rules) == 0:
+        await msg.edit(content=i18n[localization_key]["no_rules"].format(subreddit))
+        return
+
+    for rule in rules:
+        # The value field is not allowed to be a blank string
+        # So we just repeat the name of the rule if it is not provided
+        embed.add_field(
+            name=rule.short_name,
+            value=rule.description or rule.short_name,
+            inline=False,
+        )
+
+    delay = datetime.now() - start_time
+    await msg.edit(
+        content=i18n["rules"]["embed_message"].format(
+            f"{delay.microseconds // 1000} ms"
+        ),
+        embed=embed,
+    )
+
+
 class Rules(Cog):
     def __init__(self, bot: ButtercupBot, reddit_api: asyncpraw.Reddit) -> None:
         """Initialize the Rules cog."""
         self.bot = bot
         self.reddit_api = reddit_api
+
+    async def _get_rule_list(
+        self, msg: SlashMessage, subreddit: str
+    ) -> Optional[List[Rule]]:
+        """Get the list of rules for the given subreddit."""
+        try:
+            sub = await self.reddit_api.subreddit(subreddit)
+            return [rule async for rule in sub.rules]
+        except Redirect:
+            # The subreddit does not exist
+            await msg.edit(content=i18n["rules"]["sub_not_found"].format(subreddit))
+        except NotFound:
+            # A character in the sub name is not allowed
+            await msg.edit(content=i18n["rules"]["sub_not_found"].format(subreddit))
+        except Forbidden:
+            # The subreddit is private
+            await msg.edit(content=i18n["rules"]["sub_private"].format(subreddit))
+
+        return None
 
     @cog_ext.cog_slash(
         name="rules",
@@ -72,51 +124,17 @@ class Rules(Cog):
     )
     async def _rules(self, ctx: SlashContext, subreddit: str) -> None:
         """Get the rules of the specified subreddit."""
-        # Send a quick response
-        # We will edit this later with the actual content
         start = datetime.now()
         sub_name = extract_sub_name(subreddit)
+        # Send a quick response
+        # We will edit this later with the actual content
         msg = await ctx.send(i18n["rules"]["getting_rules"].format(sub_name))
 
-        sub = await self.reddit_api.subreddit(sub_name)
-
-        embed = Embed(title=i18n["rules"]["embed_title"].format(sub_name))
-
-        try:
-            rules = [rule async for rule in sub.rules]
-
-            if len(rules) == 0:
-                await msg.edit(content=i18n["rules"]["no_rules"].format(sub_name))
-                return
-
-            async for rule in sub.rules:
-                # The value field is not allowed to be a blank string
-                # So we just repeat the name of the rule if it is not provided
-                embed.add_field(
-                    name=rule.short_name,
-                    value=rule.description or rule.short_name,
-                    inline=False,
-                )
-        except Redirect:
-            # Sometimes Reddit redirects to the subreddit search
-            await msg.edit(content=i18n["rules"]["sub_not_found"].format(sub_name))
-            return
-        except NotFound:
-            # Sometimes it throws a not found exception, e.g. if a character isn't allowed
-            await msg.edit(content=i18n["rules"]["sub_not_found"].format(sub_name))
-            return
-        except Forbidden:
-            # The subreddit is private
-            await msg.edit(content=i18n["rules"]["sub_private"].format(sub_name))
+        rules = await self._get_rule_list(msg, subreddit)
+        if rules is None:
             return
 
-        delay = datetime.now() - start
-        await msg.edit(
-            content=i18n["rules"]["embed_message"].format(
-                f"{delay.microseconds // 1000} ms"
-            ),
-            embed=embed,
-        )
+        await send_rules_message(msg, rules, subreddit, start, "rules")
 
     @cog_ext.cog_slash(
         name="pirules",
@@ -133,57 +151,18 @@ class Rules(Cog):
     )
     async def _pi_rules(self, ctx: SlashContext, subreddit: str) -> None:
         """Get the rules of the specified subreddit regarding personal information."""
-        # Send a quick response
-        # We will edit this later with the actual content
         start = datetime.now()
         sub_name = extract_sub_name(subreddit)
+        # Send a quick response
+        # We will edit this later with the actual content
         msg = await ctx.send(i18n["pi_rules"]["getting_rules"].format(sub_name))
 
-        sub = await self.reddit_api.subreddit(sub_name)
-
-        embed = Embed(title=i18n["pi_rules"]["embed_title"].format(sub_name))
-
-        try:
-            rules = [rule async for rule in sub.rules]
-
-            if len(rules) == 0:
-                await msg.edit(content=i18n["pi_rules"]["no_rules"].format(sub_name))
-                return
-
-            pi_rules = [rule for rule in rules if is_pi_rule(rule)]
-
-            if len(pi_rules) == 0:
-                await msg.edit(content=i18n["pi_rules"]["no_pi_rules"].format(sub_name))
-                return
-
-            for rule in pi_rules:
-                # The value field is not allowed to be a blank string
-                # So we just repeat the name of the rule if it is not provided
-                embed.add_field(
-                    name=rule.short_name,
-                    value=rule.description or rule.short_name,
-                    inline=False,
-                )
-        except Redirect:
-            # Sometimes Reddit redirects to the subreddit search
-            await msg.edit(content=i18n["pi_rules"]["sub_not_found"].format(sub_name))
+        rules = await self._get_rule_list(msg, subreddit)
+        if rules is None:
             return
-        except NotFound:
-            # Sometimes it throws a not found exception, e.g. if a character isn't allowed
-            await msg.edit(content=i18n["pi_rules"]["sub_not_found"].format(sub_name))
-            return
-        except Forbidden:
-            # The subreddit is private
-            await msg.edit(content=i18n["pi_rules"]["sub_private"].format(sub_name))
-            return
+        rules = [rule for rule in rules if is_pi_rule(rule)]
 
-        delay = datetime.now() - start
-        await msg.edit(
-            content=i18n["pi_rules"]["embed_message"].format(
-                f"{delay.microseconds // 1000} ms"
-            ),
-            embed=embed,
-        )
+        await send_rules_message(msg, rules, subreddit, start, "pi_rules")
 
 
 def setup(bot: ButtercupBot) -> None:
