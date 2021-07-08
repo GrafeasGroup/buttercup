98251a8579d5e0d8279cb584de4e8e37f168fffc
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-07-08 09:27:38 +0200

    Add flair rank definitions

--- buttercup/cogs/__init__.py
+++ buttercup/cogs/__init__.py
@@ -18,3 +18,20 @@ plt.rcParams["ytick.color"] = line_color
 plt.rcParams["grid.color"] = line_color
 plt.rcParams["grid.alpha"] = 0.8
 plt.rcParams["figure.dpi"] = 200.0
+
+# Official flair ranks
+# Maybe we'll have this in the API one day,
+# but we need to define the colors client-side anyway
+ranks = {
+    "Initiate": {"threshold": 1, "color": "#ffffff"},
+    "Green": {"threshold": 50, "color": "#00ff00"},
+    "Teal": {"threshold": 100, "color": "#00cccc"},
+    "Purple": {"threshold": 250, "color": "#ff67ff"},
+    "Gold": {"threshold": 500, "color": "#ffd700"},
+    "Diamond": {"threshold": 1000, "color": "#add8e6"},
+    "Ruby": {"threshold": 2500, "color": "#ff7ac2"},
+    "Topaz": {"threshold": 5000, "color": "#ff7d4d"},
+    "Jade": {"threshold": 10000, "color": "#31c831"},
+    # This rank is not official yet, but we need it for predictions
+    "Sapphire": {"threshold": 25000, "color": "#99afef"},
+}
