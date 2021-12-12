8d4476e9bc4d54581900e8ad115c057280962ec1
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-12 22:14:04 +0100

    Use find endpoint to get data

--- buttercup/cogs/find.py
+++ buttercup/cogs/find.py
@@ -1,13 +1,15 @@
+from datetime import datetime
 from typing import Dict, Optional
 from urllib.parse import urlparse
 
-from blossom_wrapper import BlossomAPI, BlossomStatus
+from blossom_wrapper import BlossomAPI
 from discord import Color, Embed
 from discord.ext.commands import Cog
 from discord_slash import SlashContext, cog_ext
 from discord_slash.utils.manage_commands import create_option
 
 from buttercup.bot import ButtercupBot
+from buttercup.cogs.helpers import get_duration_str
 from buttercup.strings import translation
 
 i18n = translation()
@@ -147,60 +149,6 @@ class Find(Cog):
 
         return embed
 
-    def get_transcription(self, data: Dict) -> Dict:
-        """Get the target transcription from Blossom."""
-        for option in data["submission"]["transcription_set"]:
-            response = self.blossom_api.get_transcription(id=get_id_from_url(option))
-            if response.data[0]["author"] == data["submission"].get("completed_by"):
-                data["transcription"] = response.data[0]
-            elif get_id_from_url(response.data[0]["author"]) == 3:
-                # it's transcribot
-                data["ocr"] = response.data[0]
-
-        return data
-
-    def get_author(self, data: Dict) -> Dict:
-        """Retrieve the author from the submission or the transcription."""
-        if data.get("transcription"):
-            source_url = data["transcription"]["author"]
-        else:
-            source_url = data["submission"]["claimed_by"]
-        data["author"] = self.blossom_api.get(
-            "volunteer/", params={"id": get_id_from_url(source_url)}
-        ).json()["results"][0]
-        return data
-
-    def get_submission_from_url(self, reddit_url: str, data: Dict) -> Dict:
-        """
-        Try to get the submission corresponding to the given Reddit URL.
-
-        The URL can be a link to either a post on the partner sub, a post
-        on r/ToR, or it can be a transcription link.
-        """
-        # Determine what kind of link we're dealing with:
-        if len(reddit_url.split("/")) <= 9:
-            # It's a link to a submission, either on ToR or a partner
-            urltype = "tor_url" if "TranscribersOfReddit" in reddit_url else "url"
-            result = self.blossom_api.get_submission(**{urltype: reddit_url})
-        else:
-            # It's a comment on a partner sub, i.e. a transcription
-            # This means that the path is longer, because of the added comment ID
-            tr_response = self.blossom_api.get_transcription(url=reddit_url)
-            if tr_response.status == BlossomStatus.not_found:
-                data["submission"] = None
-                return data
-            data["transcription"] = tr_response.data[0]
-
-            # We don't have direct access to the submission ID, so we need to
-            # extract it from the submission URL
-            submission_id = get_id_from_url(data["transcription"]["submission"])
-            result = self.blossom_api.get_submission(id=submission_id)
-
-        data["submission"] = (
-            result.data[0] if result.status == BlossomStatus.ok else None
-        )
-        return data
-
     @cog_ext.cog_slash(
         name="find",
         description="Find a post given a Reddit URL.",
@@ -216,43 +164,24 @@ class Find(Cog):
     )
     async def _find(self, ctx: SlashContext, reddit_url: str) -> None:
         """Find the post with the given URL."""
-        if (normalized_url := normalize_url(reddit_url)) is None:
-            await ctx.send(
-                f"Sorry, <{reddit_url}> doesn't look like a URL I can work with."
-                + " "
-                + i18n["find"]["please_check_url"]
-            )
-            return
+        start = datetime.now()
 
         # Send a first message to show that the bot is responsive.
         # We will edit this message later with the actual content.
-        msg = await ctx.send(f"Looking for post <{normalized_url}>...")
+        msg = await ctx.send(i18n["find"]["looking_for_posts"].format(url=reddit_url))
 
-        data: Dict = {}
-
-        data: Dict[str, Dict] = self.get_submission_from_url(normalized_url, data)
-        if data["submission"] is None:
-            await msg.edit(
-                content=(
-                    f"Sorry, I couldn't find a post with the URL <{normalized_url}>."
-                    + " "
-                    + i18n["find"]["please_check_url"]
-                )
-            )
+        find_response = self.blossom_api.get("find", data={"url": reddit_url})
+        if not find_response.ok:
+            await msg.edit(content=i18n["find"]["not_found"].format(url=reddit_url))
             return
+        data = find_response.json()
 
-        await msg.edit(content="I found the post!", embed=self.to_embed(data))
-
-        # If we started with a transcription link, this will be set already.
-        if "transcription" not in data:
-            data: Dict[str, Dict] = self.get_transcription(data)
-
-        await msg.edit(content="I found the post!", embed=self.to_embed(data))
-
-        if data.get("transcription") or data["submission"].get("claimed_by"):
-            data: Dict[str, Dict] = self.get_author(data)
-
-        await msg.edit(content="I found the post!", embed=self.to_embed(data))
+        await msg.edit(
+            content=i18n["find"]["embed_message"].format(
+                duration=get_duration_str(start)
+            ),
+            embed=self.to_embed(data),
+        )
 
 
 def setup(bot: ButtercupBot) -> None:
